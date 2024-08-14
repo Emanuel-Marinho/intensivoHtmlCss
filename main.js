fecf52a42ee11afe0d7cdd76763f7bb29cf0d5b5
Emanuel Marinho <emanuel_sales12@hotmail.com>
2024-08-14 15:53:37 -0300

    Projeto intensivo Html e Css

---
+++ main.js
@@ -0,0 +1,73 @@
+// 1 -
+
+// alert('Hello World!')
+
+//2 - 
+
+// let number1 = 5
+// let number2 = 8
+
+// alert(number1 + number2)
+
+// 3 -
+
+// let tryNumber = 5
+
+// let itsNumber = typeof tryNumber == 'number'
+
+// if(itsNumber) {
+//     alert('É um número')
+// } else {
+//     alert('Não é um número')
+// }
+
+// 4 -
+
+// let tryString = false
+
+// let itsString = typeof tryString == 'string'
+
+// if(itsString) {
+//     alert('É uma String')
+// } else {
+//     alert('Não é uma string')
+// }
+
+// 5 -
+
+// let tryBoolean = 5
+
+// let itsBoolean = typeof tryBoolean == 'boolean'
+
+// if (itsBoolean) {
+//     alert('É uma booleano')
+// } else {
+//     alert('Não é um booleano')
+// }
+
+// 6 - 7 - 8
+
+// let number1 = 5
+// let number2 = 6
+
+// let sub = number1 - number2
+// let multi = number1 * number2
+// let div = (number1 / number2).toFixed(2)
+
+// alert(div)
+
+// 9 -
+
+// let number = Number(prompt('Escreva um número qualquer: '))
+
+// if (number % 2 == 0) {
+//     alert('É um número par')
+// } else {
+//     alert('Não é um número par')
+// }
+
+// if (number % 2 != 0) {
+//     alert('É um número impar')
+// } else {
+//     alert('Não é um número impar')
+// }
